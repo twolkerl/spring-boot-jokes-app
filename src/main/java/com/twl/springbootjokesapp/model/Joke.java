26a6bd09bcd6f04953eea189c55934c136226b75
Tiago Wolker <tiago.wolker@ntconsult.com.br>
2019-07-25 15:57:25 -0300

    Adicionando dependencia do mongodb

--- src/main/java/com/twl/springbootjokesapp/model/Joke.java
+++ src/main/java/com/twl/springbootjokesapp/model/Joke.java
@@ -2,9 +2,11 @@ package com.twl.springbootjokesapp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
+@Document
 public class Joke {
 
     private String text;
